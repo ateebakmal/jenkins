cb6ba07dd01c6dc2ee3744b2185349fd78e5afbf
Ateeb Akmal <152301429+ateebakmal@users.noreply.github.com>
2026-09-23 13:28:19 +0500

    x

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Chal gya beta!");
         System.out.println("Hello, Jenkins!");
         System.out.println("Learning CI/CD");
         System.out.println("Java build successful!");
