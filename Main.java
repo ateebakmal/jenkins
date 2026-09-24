08ea11c4e9f75404253a5c02135a4215ac025970
Ateeb Akmal <152301429+ateebakmal@users.noreply.github.com>
2026-09-24 17:33:14 +0500

    Update output messages in Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Chal gya beta!");
+        System.out.println("Chal gya!");
         System.out.println("Check krr!");
+        System.out.println("Checking jenkins access token");
         System.out.println("Hello, Jenkins!");
     }
 }
